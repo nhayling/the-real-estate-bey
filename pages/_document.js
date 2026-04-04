bfb973dbd38de0925947bc266880c4e323e428dc
Noah Hayling <nehayling@gmail.com>
2026-04-04 17:05:18 -0400

    added analytics link

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" data-theme="dark">
-      <Head />
+      <Head>
+        <script defer src="https://analytics.archipelagoapps.com/script.js" data-website-id="99dd9c60-0fbb-4c7e-807d-d55d5ec30f99"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
